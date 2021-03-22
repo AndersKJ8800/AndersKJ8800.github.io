fddccb7369cbea93aaccd7e85bc21e1adc27df35
AndersKJ8800 <58813668+AndersKJ8800@users.noreply.github.com>
2021-03-22 08:35:00 +0100

    asdfasdfasdfasf

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+function setup() {
+  frameRate(60);
+  angleMode(DEGREES);
+  createCanvas(100,100);
+  masterVolume(0.1);
+}
+
+function windowResized() {
+  
+}
+
+function draw() {
+  background(255,0,255);
+}
+
+
+function keyPressed() {
+  
+}
